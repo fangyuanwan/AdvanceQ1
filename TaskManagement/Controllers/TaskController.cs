acf960542c6f2c55a95aeb9660be1ad24482f419
fangyuanwan <fangyuanwan1989@gmail.com>
2022-07-12 15:50:36 +0800

    add rabbit

---
+++ TaskManagement/Controllers/TaskController.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using RabbitMQ.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagement.Models;
+using System.Text;
+
+namespace TaskManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly TaskDBContext _context;
+
+        public TaskController(TaskDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Task
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<task>>> GetTasks()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        // GET: api/Task/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<task>> Gettask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return task;
+        }
+
+        // PUT: api/Task/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Puttask(int id, task task)
+        {
+            if (id != task.taskID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!taskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Task
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<task>> Posttask(task task)
+        {
+            _context.Tasks.Add(task);
+            
+            var factory = new ConnectionFactory()
+            {
+                //HostName = "localhost" , 
+                //Port = 30724
+                HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST"),
+                Port = Convert.ToInt32(Environment.GetEnvironmentVariable("RABBITMQ_PORT"))
+            };
+
+            Console.WriteLine(factory.HostName + ":" + factory.Port);
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "tasks",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = task.taskDescription;
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "tasks",
+                                     basicProperties: null,
+                                     body: body);
+            }
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Gettask", new { id = task.taskID }, task);
+        }
+
+        // DELETE: api/Task/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Deletetask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool taskExists(int id)
+        {
+            return _context.Tasks.Any(e => e.taskID == id);
+        }
+    }
+}
--- TaskManagement/Startup.cs
+++ TaskManagement/Startup.cs
@@ -5,10 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TaskManagement.Models;
+using Microsoft.OpenApi.Models;
 
 namespace TaskManagement
 {
@@ -26,6 +29,12 @@ namespace TaskManagement
         {
 
             services.AddControllers();
+            services.AddDbContext<TaskDBContext>(opt =>
+                                          opt.UseInMemoryDatabase("Tasks"));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TasksApi", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +43,9 @@ namespace TaskManagement
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TaskApi v1"));
+                
             }
 
             app.UseRouting();
